c3fd65f20e465260231ebf1b106dcc6f1a4301a1
Nazlı Özçelik <nazliozcelikk@icloud.com>
2022-10-27 10:38:19 +0300

    Initial commit

---
+++ src/main/java/com/smartdelta/SmartDelta/SmartDeltaApplication.java
@@ -0,0 +1,13 @@
+package com.smartdelta.SmartDelta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDeltaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDeltaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartdelta/SmartDelta/SmartDeltaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartdelta.SmartDelta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDeltaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
